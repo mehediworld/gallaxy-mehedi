7222c3786ebaebb0b951a2d72aa0ce9ecec18cc5
farukham <farukham@gmail.com>
2014-07-12 16:35:29 +0600

    remain bug fixed

--- js/custom.js
+++ js/custom.js
@@ -58,15 +58,13 @@ $(window).load(function(){
 /*  Animated Progress Bars
 /*----------------------------------------------------*/
 
-    jQuery('.skills li').each(function () {
+    jQuery('.skillbar').each(function(){
         jQuery(this).fappear(function() {
-          jQuery(this).animate({opacity:1,left:"0px"},800);
-          var b = jQuery(this).find(".progress-bar").attr("data-width");
-          jQuery(this).find(".progress-bar").animate({
-            width: b + "%"
-          }, 1300, "linear");
-        }); 
-    }); 
+            jQuery(this).find('.skillbar-bar').animate({
+                width:jQuery(this).attr('data-percent')
+            },5000);
+        });
+    });
 
 
 /*----------------------------------------------------*/
